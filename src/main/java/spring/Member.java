f82f181225f83348d3f48487abe35bb2210619d7
Domino419 <noris419@naver.com>
2025-01-02 00:31:44 +0900

    로그인 처리를 위한 코드 작업 중

--- src/main/java/spring/Member.java
+++ src/main/java/spring/Member.java
@@ -48,4 +48,16 @@ public class Member {
 		this.password = newPassword;
 	}
 
+	/**
+	 * method        : matchPassword
+	 * date          : 25-01-02
+	 * param         : String password - 비교할 비밀번호
+	 * return        : Boolean - 비밀번호가 일치하면 true, 그렇지 않으면 false
+	 * description   : 현재 객체의 비밀번호와 주어진 비밀번호를 비교하여 일치 여부를 반환한다.
+	 * (sp-chap13.354page)
+	 */
+	public Boolean matchPassword(String password) {
+		return this.password.equals(password);
+	}
+
 }
